a53ee7d645a054de1073005e4a089dd48f42a5e6
chaseford <chaseford@u.boisestate.edu>
2018-04-30 21:18:08 -0600

    FinalApp_GetSchooled
    Unity Final App for GitHub

---
+++ Assets/_Complete-Game/Scripts/Done_GameController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Done_GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText highScoreText;
+	
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	void Update ()
+	{
+		if (restart)
+		{
+			if (CrossPlatformInputManager.GetButton("Shoot"))
+			{
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+	
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+			
+			if (gameOver)
+			{
+				restartText.text = "Juice Up to Try Again!";
+				restart = true;
+				break;
+			}
+		}
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore ()
+	{
+		if (score > HighScore.score) {
+			HighScore.score = score;
+			scoreText.text = "Score: " + score;
+		}   	
+		else {
+		scoreText.text = "Score: " + score;
+		}
+	}
+	
+	public void GameOver ()
+	{
+		gameOverText.text = "YOU GOT LEARNED";
+		gameOver = true;
+	}
+}
